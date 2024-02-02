09a7efd04901d979e4ed44341d8da77034dde974
Master-Bobba <bayvazov94@gmail.com>
2024-02-02 14:21:02 +0000

    some changes to db stuff

--- XSS/src/main/resources/data.sql
+++ XSS/src/main/resources/data.sql
@@ -1,5 +1,6 @@
-;--INSERT INTO POST(TITLE, USER_NAME, CONTENT)
---VALUES
---    ('Hello From Down Under', 'Nicola M.', 'It is cool in Australia. Jeremy says Hi!'),
---    ('Built Different', 'Bobby', 'Benching 225 like its NOTHING'),
---    ('New Acc, who this?', 'Alice Yu', 'This is my first post on here');
+INSERT INTO POST(TITLE, USER_NAME, CONTENT)
+VALUES
+    ('Hello From Down Under', 'Nicola M.', 'It is cool in Australia. Jeremy says Hi!'),
+    ('Built Different', 'Bobby', 'Benching 225 like its NOTHING'),
+    ('New Acc, who this?', 'Alice Yu', 'This is my first post on here'),
+    ('AWS', 'Master_bobba', 'This actually works');
